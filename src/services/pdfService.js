1f57cc2724d6f96c0921e46d8f3aee2f28e7cbdf
pranaysalunke5 <pranaysalunke5@gmail.com>
2025-11-11 00:53:11 +0530

    cmd

--- src/services/pdfService.js
+++ src/services/pdfService.js
@@ -3,6 +3,19 @@ const path = require('path');
 const puppeteer = require('puppeteer');
 const handlebars = require('handlebars');
 
+handlebars.registerHelper('eq', function (a, b, options) {
+if (!options || typeof options.fn !== 'function' || typeof options.inverse !== 'function') {
+    console.error("Handlebars 'eq' helper called incorrectly. Missing fn/inverse.");
+    return ''; 
+}
+  
+  if (a === b) {
+    return options.fn(this); 
+  }
+  return options.inverse(this); 
+});
+
+
 async function renderHtmlFromTemplate(templateName, data = {}) {
   const templatePath = path.join(process.cwd(), 'src', 'templates', `${templateName}.hbs`);
   const source = fs.readFileSync(templatePath, 'utf8');
